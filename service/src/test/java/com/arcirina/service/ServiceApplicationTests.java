e5bd1e3c688acb40ca6c736ff60acfec30a51df2
arcirina <irinaarc0712@gmail.com>
2024-07-05 14:27:21 +0500

    Version 1

---
+++ service/src/test/java/com/arcirina/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcirina.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
